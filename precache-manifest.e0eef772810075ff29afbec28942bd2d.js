ff2f3eb1e69397fdb0b0b559836801970fc5a317
nirbhay <pherwani37@gmail.com>
2018-12-06 02:11:06 -0500

    Updates

---
+++ precache-manifest.e0eef772810075ff29afbec28942bd2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pothole-detection/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d85dab5ce2fa6e7017b2",
+    "url": "/pothole-detection/static/js/runtime~main.d85dab5c.js"
+  },
+  {
+    "revision": "823f041d4b96919a54b2",
+    "url": "/pothole-detection/static/js/main.823f041d.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/pothole-detection/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "823f041d4b96919a54b2",
+    "url": "/pothole-detection/static/css/main.af872b3d.chunk.css"
+  },
+  {
+    "revision": "01d720898ffc04ff3b88d09c30bcca59",
+    "url": "/pothole-detection/index.html"
+  }
+];
