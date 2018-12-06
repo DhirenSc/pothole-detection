8f87ecfa19f27f41ba3f81e2f2c4f9d638134ec2
nirbhay <pherwani37@gmail.com>
2018-12-06 15:58:40 -0500

    Updates

--- precache-manifest.5a2dfc31c34d010aa5365da2e9ebb798.js
+++ precache-manifest.5a2dfc31c34d010aa5365da2e9ebb798.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/pothole-detection/static/js/runtime~main.d85dab5c.js"
   },
   {
-    "revision": "a107c6d03ed172318781",
-    "url": "/pothole-detection/static/js/main.a107c6d0.chunk.js"
+    "revision": "11ad8fe1f6171dd4090a",
+    "url": "/pothole-detection/static/js/main.11ad8fe1.chunk.js"
   },
   {
     "revision": "14f86c2e8e13c6706ee3",
     "url": "/pothole-detection/static/js/1.14f86c2e.chunk.js"
   },
   {
-    "revision": "a107c6d03ed172318781",
+    "revision": "11ad8fe1f6171dd4090a",
     "url": "/pothole-detection/static/css/main.7bcbf6d6.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/pothole-detection/static/css/1.9fbc88e7.chunk.css"
   },
   {
-    "revision": "27be8830084187787bdcb9ce55f57913",
+    "revision": "dad55e695736f989d24822ecef48f8c8",
     "url": "/pothole-detection/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pothole-detection/precache-manifest.2be45c1aa6a3283fe8736c630015742f.js"
+  "/pothole-detection/precache-manifest.5a2dfc31c34d010aa5365da2e9ebb798.js"
 );
 
 workbox.clientsClaim();
