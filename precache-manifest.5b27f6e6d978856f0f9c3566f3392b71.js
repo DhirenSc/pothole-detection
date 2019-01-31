9b5df9016bc3268376e677e0b8f87308294e39f0
nirbhay <pherwani37@gmail.com>
2019-01-31 17:43:16 -0500

    Updates

--- precache-manifest.5b27f6e6d978856f0f9c3566f3392b71.js
+++ precache-manifest.5b27f6e6d978856f0f9c3566f3392b71.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/pothole-detection/static/js/runtime~main.d85dab5c.js"
   },
   {
-    "revision": "316c47977bf7c15ef3fd",
-    "url": "/pothole-detection/static/js/main.316c4797.chunk.js"
+    "revision": "1bad9e4eafa384aeb64c",
+    "url": "/pothole-detection/static/js/main.1bad9e4e.chunk.js"
   },
   {
     "revision": "14f86c2e8e13c6706ee3",
     "url": "/pothole-detection/static/js/1.14f86c2e.chunk.js"
   },
   {
-    "revision": "316c47977bf7c15ef3fd",
+    "revision": "1bad9e4eafa384aeb64c",
     "url": "/pothole-detection/static/css/main.7bcbf6d6.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/pothole-detection/static/css/1.9fbc88e7.chunk.css"
   },
   {
-    "revision": "494a3a9bde08438702df46fe75f88033",
+    "revision": "2d64670f49be3fe040d324be73067342",
     "url": "/pothole-detection/index.html"
   }
 ];
